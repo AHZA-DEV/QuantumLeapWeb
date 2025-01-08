b940d27d3b387db86f39f38586ad4bf83167d1ef
AHZA-DEV <kentdiyo@gmail.com>
2025-01-08 20:04:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,55 +13,76 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // ketika window >= 640px
+        640: {
+            slidesPerView: 2,
         },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+        // ketika window >= 1024px
+        1024: {
+            slidesPerView: 3,
         },
-        breakpoints: {
-            // ketika window >= 640px
-            640: {
-                slidesPerView: 2,
-            },
-            // ketika window >= 1024px
-            1024: {
-                slidesPerView: 3,
-            },
-        },
-    });
+    },
+});
 
 
 
 // Tunggu sampai DOM selesai dimuat
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Ambil elemen yang diperlukan
     const hamburgerButton = document.getElementById('hamburger-button');
     const navMenu = document.getElementById('nav-menu');
 
     // Toggle menu saat hamburger diklik
-    hamburgerButton.addEventListener('click', function(e) {
+    hamburgerButton.addEventListener('click', function (e) {
         e.stopPropagation();
         navMenu.classList.toggle('active');
     });
 
     // Tutup menu saat mengklik di luar menu
-    document.addEventListener('click', function(event) {
+    document.addEventListener('click', function (event) {
         if (!navMenu.contains(event.target) && !hamburgerButton.contains(event.target)) {
             navMenu.classList.remove('active');
         }
     });
 
     // Tutup menu saat ukuran window berubah ke desktop
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         if (window.innerWidth >= 768) {
             navMenu.classList.remove('active');
         }
     });
 });
+
+
+
+
+
+
+
+function showCustomAlert() {
+    const alertBox = document.getElementById('custom-alert');
+    alertBox.classList.remove('hidden'); // Menampilkan alert
+
+    // Sembunyikan alert setelah 3 detik
+    setTimeout(() => {
+        hideCustomAlert();
+    }, 3000);
+}
+
+function hideCustomAlert() {
+    const alertBox = document.getElementById('custom-alert');
+    alertBox.classList.add('hidden'); // Menyembunyikan alert
+}
