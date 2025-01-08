06694ba6c127292bdf9c26fc74c730231af25ef1
AHZA-DEV <kentdiyo@gmail.com>
2025-01-08 09:56:44 +0700

    yuhuuuy
    yuhuuy

---
+++ script.js
@@ -0,0 +1,40 @@
+// JavaScript for smooth scrolling 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // ketika window >= 640px
+            640: {
+                slidesPerView: 2,
+            },
+            // ketika window >= 1024px
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+
+
